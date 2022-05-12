ad90241981a5df4a7423829ac99ba77585088e3e
odomfavour <ogochukwuodom@gmail.com>
2022-05-12 01:32:41 +0100

    ch: add live links

--- src/components/ProgramData/ProgramData.js
+++ src/components/ProgramData/ProgramData.js
@@ -17,7 +17,7 @@ export const programs = [
         "name" : "Reservoir Performance Analysis",
         "description" : "This course is designed to equip participants with the understanding of reservoir performance analysis and introduces them to material balance and decline curve analysis methods of assess reservoir performance. ",
         "image" : reservoir,
-        "price": 5000,
+        "price": 1000,
         // "note": "<h4> Who is this training for?</h4>\n <p>This training is structured to provide an accelerated learning path for engineers who aspire to have a firm grip of petroleum engineering.</p> \n <p>The following professionals can benefit from the boot camp:</p> \n <ul> <li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li> </ul>\n <h4>What are the objectives?</h4>\n <p>At the end of this training participants will:</p>\n <ul> <li> Develop dexterity in reservoir performance analysis </li> <li>Understand the physics underpinning material balance and decline curve analysis </li> <li>Use material balance and DCA methods to efficiently monitor the reservoir performance</li> <li>Determine the parameters that impact well/reservoir performance</li> <li>Use of fractional flow equations </li> <li> Use python programming to perform reservoir analysis</li> </ul>",
         "objectives": "<li> Develop dexterity in reservoir performance analysis </li> <li>Understand the physics underpinning material balance and decline curve analysis </li> <li>Use material balance and DCA methods to efficiently monitor the reservoir performance</li> <li>Determine the parameters that impact well/reservoir performance</li> <li>Use of fractional flow equations </li> <li> Use python programming to perform reservoir analysis</li>",
         "trainingFor":"This training is structured to provide an accelerated learning path for engineers who aspire to have a firm grip of petroleum engineering.",
@@ -31,7 +31,7 @@ export const programs = [
         "name" : "Well Performance Analysis",
         "description" : "This course is designed to equip participants with the required skills to proactively analyse well and reservoir performance for production optimization. ",
         "image" : wellPerformance,
-        "price": 2000,
+        "price": 1000,
         "note": "<h4> Who is this training for?</h4>\n <p>This training is structured to provide an accelerated learning path for engineers who aspire to have a firm grip of petroleum engineering.</p> \n <p>The following professionals can benefit from the boot camp:</p> \n <ul> <li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li> </ul>\n <h4>What are the objectives?</h4>\n <p>At the end of this training participants will:</p>\n <ul> <li> Understand how to realize optimum value from the performance of their wells especially in this period of low oil prices. </li> <li>Retreat to the fundamentals of well performance analysis and embed best practices in well and reservoir management (WRM) to guaranty value from their assets</li> <li>•	Understand industry standard workflow for production optimization and well intervention candidates identification/screening.</li> </ul>",
         "professionals": "<li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li>",
         // "link": "https://paystack.com/buy/well-performance-analysis-qcmooi"
@@ -42,7 +42,7 @@ export const programs = [
         "name" : "Machine Learning",
         "description" : "This course explores the basic concepts and techniques of python programming, Machine Learning fundamentals and the industrial application of machine learning in core areas of petroleum engineering like drilling, reservoir engineering, well integrity etc.",
         "image" : machineLearning,
-        "price": 8000,
+        "price": 1000,
         "note": "<h4> Who is this training for?</h4>\n <p>This training is structured to provide an accelerated learning path for engineers who aspire to have a firm grip of petroleum engineering.</p> \n <p>The following professionals can benefit from the boot camp:</p> \n <ul> <li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li> </ul>\n <h4>What are the objectives?</h4>\n <p>At the end of this training participants will:</p>\n <ul> <li>Provide participants with basic python programming concepts  </li> <li>Provide participants with firm understanding of machine learning algorithms and their applications</li> <li>Demonstrate machine learning real life applications in petroleum engineering</li> <li>Provide students with hands-on that will enable them build machine learning models </li> </ul>",
         "professionals": "<li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li>",
         "link": "https://paystack.com/buy/machine-learning-rmocbz"
@@ -52,7 +52,7 @@ export const programs = [
         "name" : "Petroleum Engineering Bootcamp",
         "description" : "This course focuses on the engineering bootcamp.",
         "image" : bootcamp,
-        "price": 4500,
+        "price": 1000,
         "note": "<h4> Who is this training for?</h4>\n <p>This training is structured to provide an accelerated learning path for engineers who aspire to have a firm grip of petroleum engineering.</p> \n <p>The following professionals can benefit from the boot camp:</p> \n <ul> <li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li> </ul>\n <h4>What are the objectives?</h4>\n <p>At the end of this training participants will:</p>\n <ul> <li> Understand how to realize optimum value from the performance of their wells especially in this period of low oil prices. </li> <li>Retreat to the fundamentals of well performance analysis and embed best practices in well and reservoir management (WRM) to guaranty value from their assets</li> <li>•	Understand industry standard workflow for production optimization and well intervention candidates identification/screening.</li> </ul>",
         "professionals": "<li>Reservoir engineers</li> <li>Geologists</li> <li>Petrophysicists</li> <li>Production Technology Engineers</li> <li>Production Technicians</li> <li> Completion and Well Intervention Engineers</li>",
         "link": "https://paystack.com/buy/petroleum-engineering-bootcamp-izzcme"
